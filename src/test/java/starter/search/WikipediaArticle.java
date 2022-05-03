d943415eedb3e7d5bc8c3fe376d9b0a8e9b6e080
hershak <gael.houl@gmail.com>
2022-05-03 09:59:57 -0500

    refactoring template to maven only and fix test error

--- src/test/java/starter/search/WikipediaArticle.java
+++ src/test/java/starter/search/WikipediaArticle.java
@@ -1,7 +1,9 @@
 package starter.search;
 
+import net.serenitybdd.core.annotations.findby.By;
 import net.serenitybdd.screenplay.targets.Target;
 
 public class WikipediaArticle {
-    public static final Target HEADING =  Target.the("article identifier").locatedBy("p.mw-search-createlink a");
+    //public static final Target HEADING =  Target.the("article identifier").locatedBy("p.mw-search-createlink a");
+    public static final Target HEADING =  Target.the("article identifier").located(By.id("firstHeading"));
 }
